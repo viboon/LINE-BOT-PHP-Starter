3272d3abba33224c7c6f73f55eec41a0bdb278e3
viboon <viboonset@gmail.com>
2017-06-26 11:32:01 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -11,7 +11,7 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-if($arrJson['events'][0]['message']['text'] == "้hi"){
+if($arrJson['events'][0]['message']['text'] == "hi"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
