b049dbcbefb2fc706d4b36f222ceec6b400ff243
viboon <viboonset@gmail.com>
2017-06-27 10:19:49 +0700

    Update callbackImgur.php

--- callbackImgur.php
+++ callbackImgur.php
@@ -1,26 +1,26 @@
 <?php
-$client_id = 'xxxxxxxx';
-
-$file = file_get_contents("test-image.png");
-
-$url = 'https://api.imgur.com/3/image.json';
-$headers = array("Authorization: Client-ID $client_id");
-$pvars  = array('image' => base64_encode($file));
-
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-   CURLOPT_URL=> $url,
-   CURLOPT_TIMEOUT => 30,
-   CURLOPT_POST => 1,
-   CURLOPT_RETURNTRANSFER => 1,
-   CURLOPT_HTTPHEADER => $headers,
-   CURLOPT_POSTFIELDS => $pvars
-));
-
-$json_returned = curl_exec($curl); // blank response
-echo "Result: " . $json_returned ;
-
-curl_close ($curl); 
-
+$strAccessToken = "Wu8bFpFwg2PwKmCbE4Bft4m9Hti9UmNdgYuotBQ+zGNVEr7UiCYHRq8PR+zV7L1h5fNB1W8zeBltG7ZY9lUSJu87HYmmqvrRuZZ+AkVFv9FcfG1NTWPMUDv5QuZiFEx/9lGso+b6NKsqyGOC7iS8xAdB04t89/1O/w1cDnyilFU=";
+ 
+$strUrl = "https://api.line.me/v2/bot/message/push";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+$arrPostData = array();
+$arrPostData['to'] = "U3488e5a969044cd8eaaa2b26bfadfe04";
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "image upload completed.";
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
 ?>
