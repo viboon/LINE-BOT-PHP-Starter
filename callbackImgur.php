954c103dcd3e17cbe4e58f503819d75e2d68a068
viboon <viboonset@gmail.com>
2017-06-27 10:11:21 +0700

    Create callbackImgur.php

---
+++ callbackImgur.php
@@ -0,0 +1,26 @@
+<?php
+$client_id = 'xxxxxxxx';
+
+$file = file_get_contents("test-image.png");
+
+$url = 'https://api.imgur.com/3/image.json';
+$headers = array("Authorization: Client-ID $client_id");
+$pvars  = array('image' => base64_encode($file));
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+   CURLOPT_URL=> $url,
+   CURLOPT_TIMEOUT => 30,
+   CURLOPT_POST => 1,
+   CURLOPT_RETURNTRANSFER => 1,
+   CURLOPT_HTTPHEADER => $headers,
+   CURLOPT_POSTFIELDS => $pvars
+));
+
+$json_returned = curl_exec($curl); // blank response
+echo "Result: " . $json_returned ;
+
+curl_close ($curl); 
+
+?>
