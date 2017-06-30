e1a4cb8e2d9db388e6fdbe6443f6a0e38c021b20
viboon <viboonset@gmail.com>
2017-06-30 14:04:50 +0700

    Update webhookTFEX.php

--- webhookTFEX.php
+++ webhookTFEX.php
@@ -16,7 +16,7 @@ if($arrJson['events'][0]['message']['text'] == "hi"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-  sendUserID("User Name: ".$arrJson['events'][0]['source']['type']."\n New UserID: ".$arrJson['events'][0]['source']['userId']);
+  sendUserID("User Name: ".$arrJson['events'][0]['source']['timestamp']."\n New UserID: ".$arrJson['events'][0]['source']['userId']);
 }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
